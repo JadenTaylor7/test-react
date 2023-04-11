9c8f3c3e1ff9420055d3fa59d027e69e102262b8
Jaden Taylor <jtjadent@gmail.com>
2023-04-11 15:57:39 -0600

    Adding a component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import FunctionDemo from "./components/FunctionDemo";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Learning how to use react effectively
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Function Style Component: <FunctionDemo who="Person!" color="Blue" />
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Learning how to use react effectively
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
   );
 }
 
